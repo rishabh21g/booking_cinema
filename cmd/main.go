f84b9a493c25a164d3f0eba1fa3b892e5a28a0fc
rishabh21g <biasedengineer@gmail.com>
2026-04-24 11:51:20 +0530

    feat: implement movie listing functionality and JSON response utility

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/rishabh21g/booking_cinema/utils"
+)
+
+type movieResponse struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Rows        int    `json:"rows"`
+	SeatsPerRow int    `json:"seats_per_row"`
+}
+
+var movies = []movieResponse{
+	{ID: "khatta-meetha", Title: "Khatta Meetha", Rows: 5, SeatsPerRow: 8},
+	{ID: "Bhagam Bhaag", Title: "Bhagam Bhag", Rows: 4, SeatsPerRow: 6},
+}
+
+func listMovies(w http.ResponseWriter, r *http.Request) {
+	utils.WriteJSON(w, http.StatusOK, movies)
+}
+
+func main() {
+
+	// creating a new server using multiplexer
+	mux := http.NewServeMux()
+
+	// if error just log it
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+
+	mux.HandleFunc("GET /movies", listMovies)
+
+}
